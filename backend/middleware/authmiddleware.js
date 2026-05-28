7e9520370bfbfaac218d5af46e3b2b7fe7a1a774
Krishnavardhan2305 <captainkv.2511@gmail.com>
2026-05-28 22:32:23 +0530

    added frontend auth

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,4 +1,5 @@
 import jwt from jsonwebtoken;
+import BlacklistToken from "../models/blacklistmodel";
 
 export const verifyToken=async(req,res,next)=>{    
 
@@ -6,6 +7,10 @@ export const verifyToken=async(req,res,next)=>{
         const token=req.cookies.token;
         if(!token){
             return res.status(401).json({message:"Unauthorized"});
+        }
+        const isBlacklisted=await BlacklistToken.findOne({token});
+        if(isBlacklisted){
+            return res.status(401).json({message:"Token is invalid"});
         }
         const decoded=jwt.verify(token,process.env.SECRET_KEY);
         req.user=decoded;
